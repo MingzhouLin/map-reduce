26fb14090ccde9f5d6d2613f2c6be21ade57c91f
NBLMZ <mingzhoulin1994@gmail.com>
2017-11-09 19:50:42 -0500

    optimize

--- src/WordCount.java
+++ src/WordCount.java
@@ -15,15 +15,12 @@ public class WordCount {
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
       HashMap<String, Integer> frequency = new HashMap<>();
       String line = value.toString();
-      String tabDelim[] = line.split("\\s+");
       ArrayList<String> items = new ArrayList<>();
-//      for (String s : tabDelim) {
-      String[] commaDelim = tabDelim[0].split(",");
+      //seperate and sort
+      String[] commaDelim = line.split(",");
       int len = commaDelim.length;
         sort(commaDelim, items);
-//           output.collect(new Text(commaDelim[i]), new IntWritable(1));
-//      }
-      //backtracking algorithm and brute force
+      //backtracking algorithm
       if (len==1){
         support=Integer.parseInt(commaDelim[0]);
         output.collect(new Text("support:"),new IntWritable(support));
@@ -31,8 +28,9 @@ public class WordCount {
         LinkedList<String> stack = new LinkedList<>();
         for (int i = 2; i <= len - 1; i++) {
           int k = 1;
-          bruteforce(k, i, stack, tabDelim, items, 0,frequency);
+          backTracking(k, i, stack, items, 0,frequency);
         }
+        //output every item set
         for (HashMap.Entry<String, Integer> entry : frequency.entrySet()) {
           int i = 0;
           output.collect(new Text(entry.getKey()), new IntWritable(entry.getValue()));
@@ -40,13 +38,12 @@ public class WordCount {
       }
     }
 
-    private void bruteforce(int k, int i, LinkedList<String> stack, String[] commaDelim, ArrayList<String> items, int n,HashMap<String,Integer> frequency) {
+    private void backTracking(int k, int i, LinkedList<String> stack,  ArrayList<String> items, int n,HashMap<String,Integer> frequency) {
       if (k > i) {
-        int count = 0;
-        for (String s : commaDelim) {
-          if (find(stack, s)) {
-            count++;
-          }
+//        for (String s : commaDelim) {
+//          if (find(stack, s)) {
+//            count++;
+//          }
           String frequenSet = new String();
           for (String str : stack) {
             if (str.equals(stack.getLast())) {
@@ -55,12 +52,11 @@ public class WordCount {
               frequenSet = frequenSet + str + ",";
             }
           }
-          frequency.put(frequenSet, count);
-        }
+          frequency.put(frequenSet, 1);
       } else {
         for (int j = n; j < items.size(); j++) {
           stack.add(items.get(j));
-          bruteforce(k + 1, i, stack, commaDelim, items, j + 1,frequency);
+          backTracking(k + 1, i, stack, items, j + 1,frequency);
         }
       }
       if (stack.size() > 0) {
@@ -68,21 +64,21 @@ public class WordCount {
       }
     }
 
-    private boolean find(LinkedList<String> stack, String s) {
-      String[] split = s.split(",");
-      for (String str : stack) {
-        boolean find = false;
-        for (int i = 1; i < split.length; i++) {
-          if (str.equals(split[i])) {
-            find = true;
-          }
-        }
-        if (!find) {
-          return false;
-        }
-      }
-      return true;
-    }
+//    private boolean find(LinkedList<String> stack, String s) {
+//      String[] split = s.split(",");
+//      for (String str : stack) {
+//        boolean find = false;
+//        for (int i = 1; i < split.length; i++) {
+//          if (str.equals(split[i])) {
+//            find = true;
+//          }
+//        }
+//        if (!find) {
+//          return false;
+//        }
+//      }
+//      return true;
+//    }
 
     private void sort(String[] commaDelim, ArrayList<String> items) {
       for (int i = 1; i <commaDelim.length; i++) {
